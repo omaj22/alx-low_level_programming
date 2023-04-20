6b573c6bbb44a30bcb28537e9d56516113e2558f
Chioma Ezeuduji <ezeudujichioma@yahoo.com>
2023-04-20 14:37:15 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -22,29 +22,28 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-					printf("%s%c", sep, va_arg(list, int));
+					printf("%s%c", sep, va_arg(list, int)) c = 1;
 					break;
 				case 'i':
-					printf("%s%d", sep, va_arg(list, int));
+					printf("%s%d", sep, va_arg(list, int)) c = 1;
 					break;
 				case 'f':
-					printf("%s%f", sep, va_arg(list, double));
+					printf("%s%f", sep, va_arg(list, double)) c = 1;
 					break;
 				case 's':
-					str = va_arg(list, char *);
+					str = va_arg(list, char *) c = 1;
 					if (!str)
-						str = "(nil)";
-					printf("%s%s", sep, str);
+					{
+						printf("(nill)");
+						break;
+					}
+					printf("%s", str);
 					break;
-				default:
-					i++;
-					continue;
 			}
+			i++;
+		}
 			sep = ", ";
 			i++;
 		}
-	}
-
-	printf("\n");
-	va_end(list);
-
+	printf("\n"), va_end(list);
+}
